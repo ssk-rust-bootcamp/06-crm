7bb2b417b3131cb3e96509378a43cb444d2cbe1c
shikun.sh <shikun.sh@cheilpengtai.com>
2024-07-22 10:44:23 +0800

    v6-2-prost-tonic-build

---
+++ crm/src/server.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+use crm::pb::{
+    user_service_server::{UserService, UserServiceServer},
+    CreateUserRequest, GetUserRequest, User,
+};
+use tonic::{async_trait, transport::Server, Response, Status};
+
+#[derive(Default)]
+pub struct UserServer {}
+#[async_trait]
+impl UserService for UserServer {
+    async fn get_user(&self, request: tonic::Request<GetUserRequest>) -> Result<Response<User>, Status> {
+        let input = request.into_inner();
+        println!("get_user: {:?}", input);
+        Ok(Response::new(User::default()))
+    }
+    async fn create_user(&self, request: tonic::Request<CreateUserRequest>) -> Result<Response<User>, Status> {
+        let input = request.into_inner();
+        println!("create_user: {:?}", input);
+        Ok(Response::new(User::default()))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let addr = "[::1]:50051".parse().unwrap();
+    let svc = UserServer::default();
+
+    println!("UserService listening on: {}", addr);
+
+    Server::builder().add_service(UserServiceServer::new(svc)).serve(addr).await?;
+    Ok(())
+}
